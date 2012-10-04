32d9e1d200da1ccb756e9c5045af457033142383
Sébastien Bigaret <sebastien.bigaret@telecom-bretagne.eu>
2012-10-04 09:27:18 +0200

    Adaptation to match changes in the latest praxis

--- src/org/project_euclide/delegates/InitialisationDelegate.java
+++ src/org/project_euclide/delegates/InitialisationDelegate.java
@@ -4,7 +4,7 @@ public class InitialisationDelegate
     extends eu.telecom_bretagne.praxis.common.Launcher.InitialisationDelegate
 {   
 	@Override
-	public void initialize()
+	public void initialize(boolean client, boolean server, boolean platform)
 	{
 		/* left intentionally blank, nothing to do for the moment being */
 	}
